a8bb496247725bd66cce9911e3cb3706cf939f57
Aakash Kumar Singh <aakashkumarsingh824301@gmail.com>
2025-02-25 20:09:14 +0530

    final_commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const signuproutes=require('./routes/signuproutes');
-const loginroutes=require("./routes/loginroutes");
-const cors=require("cors");
-require('dotenv').config(); 
+//This is the index.js file
+const express = require("express");
+const mongoose = require("mongoose");
+const signuproutes = require("./routes/signuproutes");
+const loginroutes = require("./routes/loginroutes");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -13,11 +14,11 @@ const mongoURI = "mongodb://localhost:27017/carrental";
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Failed to connect to MongoDB:', err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
-app.use("/carrental",signuproutes);
-app.use("/carrental",loginroutes);
+app.use("/carrental", signuproutes);
+app.use("/carrental", loginroutes);
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
